5d5c2b2284ec8e267bc3dc8054b22ecfd7700c11
Ivan Chernousov <civ-viers@yandex-team.ru>
2017-07-06 13:36:52 +0300

    Directory structure fixed

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -6,6 +6,3 @@ class TestLaunch(unittest.TestCase):
     def test_basic_launch(self):
         bot_instance = ZordonBot()
         bot_instance.updater.stop()
-
-if __name__ == '__main__':
-    unittest.main()
