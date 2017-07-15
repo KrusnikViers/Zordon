d6c30aebd7f66d79fb4270a7750cd298e4dd15a2
Viers <krusnik.viers@gmail.com>
2017-07-15 17:46:59 +0300

    More tests for activity and subscriptions

--- tests/test_handlers_activity.py
+++ tests/test_handlers_activity.py
@@ -13,9 +13,18 @@ class TestActivityHandlers(BaseTestCase):
     def test_list_basic(self):
         activity = Activity.create(name='test', owner=self.user_1)
         Subscription.create(activity=activity, user=self.user_1)
+        Participant.create(activity=activity, user=self.user_1, report_time=datetime.datetime.now())
+        on_list(self._mm_bot, self._mm_update, self.user_1)
+
+    def test_list_empty(self):
         on_list(self._mm_bot, self._mm_update, self.user_1)
 
     def test_new_basic(self):
+        on_new(self._mm_bot, self._mm_update, self.user_1)
+
+    def test_new_another_pending(self):
+        self.user_1.pending_action = pending_user_actions['u_report']
+        self.user_1.save()
         on_new(self._mm_bot, self._mm_update, self.user_1)
 
     def test_new_with_data_basic(self):
@@ -23,12 +32,32 @@ class TestActivityHandlers(BaseTestCase):
         on_new_with_data(self._mm_bot, self._mm_update, self.user_1)
         Activity.get(name='test_name', owner=self.user_1)
 
+    def test_new_with_data_basic(self):
+        self.set_message_text('bad name?')
+        on_new_with_data(self._mm_bot, self._mm_update, self.user_1)
+        with self.assertRaises(Activity.DoesNotExist):
+            Activity.get(name='test_name', owner=self.user_1)
+
     def test_delete_basic(self):
         Activity.create(name='test', owner=self.user_1)
+        on_delete(self._mm_bot, self._mm_update, self.user_1)
+
+    def test_delete_empty(self):
         on_delete(self._mm_bot, self._mm_update, self.user_1)
 
     def test_delete_with_data_basic(self):
         Activity.create(name='test', owner=self.user_1)
         self.set_callback_data(self.user_1.telegram_user_id, 'a_delete test')
         on_delete_with_data(self._mm_bot, self._mm_update)
-        self.assertEqual(0, Activity.select().where(Activity.name == 'test').count())
+        self.assertFalse(Activity.select().where(Activity.name == 'test').exists())
+
+    def test_delete_with_data_no_activity(self):
+        self.set_callback_data(self.user_1.telegram_user_id, 'a_delete non_existing')
+        on_delete_with_data(self._mm_bot, self._mm_update)
+
+    def test_delete_with_data_not_enough_rights(self):
+        Activity.create(name='test', owner=self.user_1)
+        usual_user = User.create(telegram_user_id=12345)
+        self.set_callback_data(usual_user.telegram_user_id, 'a_delete test')
+        on_delete_with_data(self._mm_bot, self._mm_update)
+        self.assertTrue(Activity.select().where(Activity.name == 'test').exists())
--- tests/test_handlers_subscription.py
+++ tests/test_handlers_subscription.py
@@ -14,15 +14,35 @@ class TestParticipantHandlers(BaseTestCase):
     def test_new_basic(self):
         on_new(self._mm_bot, self._mm_update, self.user_1)
 
+    def test_new_no_activities(self):
+        Activity.delete().execute()
+        on_new(self._mm_bot, self._mm_update, self.user_1)
+
     def test_new_with_data_basic(self):
+        another_user = User.create(telegram_user_id=12345)
+        Participant.create(activity=self.activity, user=another_user, report_time=datetime.datetime.now())
         self.set_callback_data(self.user_1.telegram_user_id, 's_new ' + self.activity.name)
+        on_new_with_data(self._mm_bot, self._mm_update)
+
+    def test_new_with_data_wrong_activity(self):
+        self.set_callback_data(self.user_1.telegram_user_id, 's_new non_existing')
         on_new_with_data(self._mm_bot, self._mm_update)
 
     def test_delete_basic(self):
         Subscription.create(activity=self.activity, user=self.user_1)
         on_delete(self._mm_bot, self._mm_update, self.user_1)
 
+    def test_delete_empty(self):
+        on_delete(self._mm_bot, self._mm_update, self.user_1)
+
     def test_delete_with_data_basic(self):
         Subscription.create(activity=self.activity, user=self.user_1)
         self.set_callback_data(self.user_1.telegram_user_id, 's_delete ' + self.activity.name)
         on_delete_with_data(self._mm_bot, self._mm_update)
+
+    def test_delete_with_data_wrong_activity(self):
+        Subscription.create(activity=self.activity, user=self.user_1)
+        self.set_callback_data(self.user_1.telegram_user_id, 's_new non_existing')
+        on_new_with_data(self._mm_bot, self._mm_update)
+        self.assertTrue(Subscription.select((Subscription.activity == self.activity) &
+                                            (Subscription.user == self.user_1)).exists())
