d1c3485f0e5d73bfc29522bfa5c128c3bc2f4483
Chernousov Ivan Viers <krusnik.viers@gmail.com>
2017-11-01 16:57:19 +0300

    Add information about project

---
+++ app/base/about.py
@@ -0,0 +1,9 @@
+# Every update of this values should be tagged in git and means new version, available for deployment.
+version_major = 1
+version_minor = 0
+version_patch = 0
+
+version = '.'.join(map(str, [version_major, version_minor, version_patch]))
+
+name = 'Zordon Bot'
+full_name = name + ' v' + version
