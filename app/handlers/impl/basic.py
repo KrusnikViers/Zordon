294a880ac72baa48228e4fd7802a44e6d210aa54
padme <padme@yandex-team.ru>
2018-11-14 20:54:28 +0300

    Click here if you not afraid

--- app/handlers/impl/basic.py
+++ app/handlers/impl/basic.py
@@ -29,3 +29,7 @@ def process_group_changes(context: Context):
 def on_help_or_start(context: Context):
     message_template = _('{project}_help_for_group') if context.group else _('{project}_help_for_private')
     context.send_response_message(message_template.format(project=PROJECT_FULL_NAME))
+
+
+def on_click_here(context: Context):
+    context.send_response_message(_('rdr2_easter_egg'))
