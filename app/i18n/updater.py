e7ac7bf6ffa83fcbecd42b1563e088461985a144
Viers <krusnik.viers@gmail.com>
2018-11-14 21:10:49 +0300

    Check translations for fuzzyness

--- app/i18n/updater.py
+++ app/i18n/updater.py
@@ -24,7 +24,8 @@ class TranslationsUpdater:
         for language in SUPPORTED_LANGUAGES:
             self.locale_dir.joinpath(language, 'LC_MESSAGES').mkdir(parents=True, exist_ok=True)
             updated_catalog = self._get_updated_translations(language, new_catalog)
-            is_complete = is_complete and self._is_translations_complete(updated_catalog, language)
+            is_lang_translations_complete = self._is_translations_complete(updated_catalog, language)
+            is_complete = is_complete and is_lang_translations_complete
             with self.locale_dir.joinpath(language, 'zordon.po').open(mode='wb') as file:
                 pofile.write_po(file, updated_catalog, sort_output=True, width=None, omit_header=True)
             with self.locale_dir.joinpath(language, 'LC_MESSAGES', 'zordon.mo').open(mode='wb') as file:
@@ -54,7 +55,6 @@ class TranslationsUpdater:
         if current_catalog.obsolete:
             obsolete_lines = ', '.join(current_catalog.obsolete.keys())
             logging.warning('Obsolete lines found in {lang}: {lines}'.format(lang=language, lines=obsolete_lines))
-        catalog.fuzzy = False
         return current_catalog
 
     def _read_existing_translations_for_language(self, language: str) -> catalog.Catalog:
@@ -68,11 +68,16 @@ class TranslationsUpdater:
     def _is_translations_complete(strings_catalog: catalog.Catalog, language: str) -> bool:
         is_translations_valid = True
         for msg in strings_catalog:
+            if not msg.id:
+                continue
             if not msg.string:
                 logging.error('Translation for {msg}({lang}) is missing!'.format(msg=msg.id, lang=language))
                 is_translations_valid = False
             elif msg.check():
                 errors = ', '.join(map(str, msg.check()))
                 logging.error('{msg}({lang}): {errors}'.format(msg=msg.id, lang=language, errors=errors))
+                is_translations_valid = False
+            elif msg.fuzzy:
+                logging.error('Translation for {msg}({lang}) is fuzzy!'.format(msg=msg.id, lang=language))
                 is_translations_valid = False
         return is_translations_valid
