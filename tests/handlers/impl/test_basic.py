41f548d3106471436df359065ab3c2855d8dcf22
padme <padme@yandex-team.ru>
2018-11-14 20:54:28 +0300

    Click here if you not afraid

--- tests/handlers/impl/test_basic.py
+++ tests/handlers/impl/test_basic.py
@@ -64,3 +64,8 @@ class TestBasicHandlers(InBotTestCase):
         context = MagicMock()
         basic.on_help_or_start(context)
         context.send_response_message.assert_called_once_with('Zordon v3.0.0_help_for_group')
+
+    def test_click_here(self):
+        context = MagicMock()
+        basic.on_click_here(context)
+        context.send_response_message.assert_called_once_with(_('rdr2_easter_egg'))
